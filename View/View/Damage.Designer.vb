c3b6e94ae4ae4b2554970198d22a5b5c5f8dd7c0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-06-26 04:36:16 +0900

    フォームにコントロールを配置

--- View/View/Damage.Designer.vb
+++ View/View/Damage.Designer.vb
@@ -22,17 +22,280 @@ Partial Class Damage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.cmdOK = New System.Windows.Forms.Button()
+        Me.cmdExit = New System.Windows.Forms.Button()
+        Me.grdDamage = New System.Windows.Forms.DataGridView()
+        Me.fraStatus = New System.Windows.Forms.GroupBox()
+        Me.lblAttack = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.updShield = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.updWeapon = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.updPower = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.updLevel = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.fraMode = New System.Windows.Forms.GroupBox()
+        Me.optMode0 = New System.Windows.Forms.RadioButton()
+        Me.optMode1 = New System.Windows.Forms.RadioButton()
+        Me.optMode2 = New System.Windows.Forms.RadioButton()
+        Me.cmbEnemy = New System.Windows.Forms.ComboBox()
+        CType(Me.grdDamage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.fraStatus.SuspendLayout()
+        CType(Me.updShield, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.updWeapon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.updPower, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.updLevel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.fraMode.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'cmdOK
         '
+        Me.cmdOK.Location = New System.Drawing.Point(840, 10)
+        Me.cmdOK.Name = "cmdOK"
+        Me.cmdOK.Size = New System.Drawing.Size(81, 33)
+        Me.cmdOK.TabIndex = 0
+        Me.cmdOK.Text = "計算(&C)"
+        Me.cmdOK.UseVisualStyleBackColor = True
+        '
+        'cmdExit
+        '
+        Me.cmdExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.cmdExit.Location = New System.Drawing.Point(840, 56)
+        Me.cmdExit.Name = "cmdExit"
+        Me.cmdExit.Size = New System.Drawing.Size(81, 33)
+        Me.cmdExit.TabIndex = 1
+        Me.cmdExit.Text = "終了(&X)"
+        Me.cmdExit.UseVisualStyleBackColor = True
+        '
+        'grdDamage
+        '
+        Me.grdDamage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.grdDamage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdDamage.Location = New System.Drawing.Point(8, 128)
+        Me.grdDamage.Name = "grdDamage"
+        Me.grdDamage.RowTemplate.Height = 21
+        Me.grdDamage.Size = New System.Drawing.Size(913, 569)
+        Me.grdDamage.TabIndex = 2
+        '
+        'fraStatus
+        '
+        Me.fraStatus.Controls.Add(Me.lblAttack)
+        Me.fraStatus.Controls.Add(Me.Label5)
+        Me.fraStatus.Controls.Add(Me.updShield)
+        Me.fraStatus.Controls.Add(Me.Label4)
+        Me.fraStatus.Controls.Add(Me.updWeapon)
+        Me.fraStatus.Controls.Add(Me.Label3)
+        Me.fraStatus.Controls.Add(Me.updPower)
+        Me.fraStatus.Controls.Add(Me.Label2)
+        Me.fraStatus.Controls.Add(Me.updLevel)
+        Me.fraStatus.Controls.Add(Me.Label1)
+        Me.fraStatus.Location = New System.Drawing.Point(8, 0)
+        Me.fraStatus.Name = "fraStatus"
+        Me.fraStatus.Size = New System.Drawing.Size(457, 121)
+        Me.fraStatus.TabIndex = 3
+        Me.fraStatus.TabStop = False
+        Me.fraStatus.Text = "トルネコのステータス"
+        '
+        'lblAttack
+        '
+        Me.lblAttack.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAttack.Location = New System.Drawing.Point(336, 88)
+        Me.lblAttack.Name = "lblAttack"
+        Me.lblAttack.Size = New System.Drawing.Size(113, 19)
+        Me.lblAttack.TabIndex = 13
+        Me.lblAttack.Text = "0"
+        Me.lblAttack.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label5.Location = New System.Drawing.Point(232, 88)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 19)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "攻撃力："
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'updShield
+        '
+        Me.updShield.Location = New System.Drawing.Point(336, 56)
+        Me.updShield.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.updShield.Name = "updShield"
+        Me.updShield.Size = New System.Drawing.Size(113, 19)
+        Me.updShield.TabIndex = 11
+        Me.updShield.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Location = New System.Drawing.Point(232, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(97, 19)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "盾の強さ："
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'updWeapon
+        '
+        Me.updWeapon.Location = New System.Drawing.Point(112, 56)
+        Me.updWeapon.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.updWeapon.Name = "updWeapon"
+        Me.updWeapon.Size = New System.Drawing.Size(113, 19)
+        Me.updWeapon.TabIndex = 9
+        Me.updWeapon.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(8, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 19)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "剣の強さ："
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'updPower
+        '
+        Me.updPower.Location = New System.Drawing.Point(336, 24)
+        Me.updPower.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.updPower.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.updPower.Name = "updPower"
+        Me.updPower.Size = New System.Drawing.Size(113, 19)
+        Me.updPower.TabIndex = 7
+        Me.updPower.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.updPower.Value = New Decimal(New Integer() {8, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Location = New System.Drawing.Point(232, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 19)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "ちから(&P)："
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'updLevel
+        '
+        Me.updLevel.Location = New System.Drawing.Point(111, 24)
+        Me.updLevel.Maximum = New Decimal(New Integer() {37, 0, 0, 0})
+        Me.updLevel.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.updLevel.Name = "updLevel"
+        Me.updLevel.Size = New System.Drawing.Size(113, 19)
+        Me.updLevel.TabIndex = 5
+        Me.updLevel.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.updLevel.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(8, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 19)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "レベル(&L)："
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'fraMode
+        '
+        Me.fraMode.Controls.Add(Me.cmbEnemy)
+        Me.fraMode.Controls.Add(Me.optMode2)
+        Me.fraMode.Controls.Add(Me.optMode1)
+        Me.fraMode.Controls.Add(Me.optMode0)
+        Me.fraMode.Location = New System.Drawing.Point(480, 0)
+        Me.fraMode.Name = "fraMode"
+        Me.fraMode.Size = New System.Drawing.Size(233, 121)
+        Me.fraMode.TabIndex = 14
+        Me.fraMode.TabStop = False
+        Me.fraMode.Text = "表示する内容"
+        '
+        'optMode0
+        '
+        Me.optMode0.BackColor = System.Drawing.Color.White
+        Me.optMode0.Location = New System.Drawing.Point(8, 16)
+        Me.optMode0.Name = "optMode0"
+        Me.optMode0.Size = New System.Drawing.Size(217, 17)
+        Me.optMode0.TabIndex = 15
+        Me.optMode0.TabStop = True
+        Me.optMode0.Text = "敵を攻撃した時のダメージ"
+        Me.optMode0.UseVisualStyleBackColor = False
+        '
+        'optMode1
+        '
+        Me.optMode1.BackColor = System.Drawing.Color.White
+        Me.optMode1.Location = New System.Drawing.Point(8, 40)
+        Me.optMode1.Name = "optMode1"
+        Me.optMode1.Size = New System.Drawing.Size(217, 17)
+        Me.optMode1.TabIndex = 16
+        Me.optMode1.TabStop = True
+        Me.optMode1.Text = "敵から受けるダメージ"
+        Me.optMode1.UseVisualStyleBackColor = False
+        '
+        'optMode2
+        '
+        Me.optMode2.BackColor = System.Drawing.Color.White
+        Me.optMode2.Location = New System.Drawing.Point(8, 64)
+        Me.optMode2.Name = "optMode2"
+        Me.optMode2.Size = New System.Drawing.Size(217, 17)
+        Me.optMode2.TabIndex = 17
+        Me.optMode2.TabStop = True
+        Me.optMode2.Text = "特定の敵との戦闘"
+        Me.optMode2.UseVisualStyleBackColor = False
+        '
+        'cmbEnemy
+        '
+        Me.cmbEnemy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbEnemy.FormattingEnabled = True
+        Me.cmbEnemy.Location = New System.Drawing.Point(8, 88)
+        Me.cmbEnemy.Name = "cmbEnemy"
+        Me.cmbEnemy.Size = New System.Drawing.Size(217, 20)
+        Me.cmbEnemy.TabIndex = 18
+        '
+        'Damage
+        '
+        Me.AcceptButton = Me.cmdOK
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(517, 386)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.CancelButton = Me.cmdExit
+        Me.ClientSize = New System.Drawing.Size(929, 709)
+        Me.Controls.Add(Me.fraMode)
+        Me.Controls.Add(Me.fraStatus)
+        Me.Controls.Add(Me.grdDamage)
+        Me.Controls.Add(Me.cmdExit)
+        Me.Controls.Add(Me.cmdOK)
+        Me.Location = New System.Drawing.Point(0, 56)
+        Me.Name = "Damage"
+        Me.Text = "トルネコの大冒険　戦闘ダメージ計算"
+        CType(Me.grdDamage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.fraStatus.ResumeLayout(False)
+        CType(Me.updShield, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.updWeapon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.updPower, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.updLevel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.fraMode.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents cmdOK As System.Windows.Forms.Button
+    Friend WithEvents cmdExit As System.Windows.Forms.Button
+    Friend WithEvents grdDamage As System.Windows.Forms.DataGridView
+    Friend WithEvents fraStatus As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents updLevel As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents updPower As System.Windows.Forms.NumericUpDown
+    Friend WithEvents updWeapon As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents updShield As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblAttack As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents fraMode As System.Windows.Forms.GroupBox
+    Friend WithEvents optMode1 As System.Windows.Forms.RadioButton
+    Friend WithEvents optMode0 As System.Windows.Forms.RadioButton
+    Friend WithEvents optMode2 As System.Windows.Forms.RadioButton
+    Friend WithEvents cmbEnemy As System.Windows.Forms.ComboBox
 
 End Class
